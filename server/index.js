c159d516a19f8b68084666afca0dba179648de03
irma-ahmed <ahmedirmani@yahoo.fr>
2023-10-25 14:06:02 +0100

    server and database created

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const port = 1128; // You can choose any available port
+app.use(express.json());
+require("./database/index.js")
+
+
+
+app.use(express.json());
+app.use(cors())
+
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});

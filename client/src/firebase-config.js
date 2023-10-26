d2759fd8cefdb223e04de0186c0c123ebc6a0c83
irma-ahmed <ahmedirmani@yahoo.fr>
2023-10-26 14:43:52 +0100

    repo ready

---
+++ client/src/firebase-config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCX4jPxFJQC5T1zDPzrKz3HRYQdeps2St4",
+    authDomain: "medico-5add3.firebaseapp.com",
+    projectId: "medico-5add3",
+    storageBucket: "medico-5add3.appspot.com",
+    messagingSenderId: "468442720217",
+    appId: "1:468442720217:web:4346fe0419451e1fe39bc8",
+    measurementId: "G-V475XKKJSM"
+  };
+
+  const app = initializeApp(firebaseConfig);
+
+  export default  getFirestore(app);

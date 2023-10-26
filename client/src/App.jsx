30302312be5fb32a47f667f1d5306fa8cf26b9fe
Le Ash <ksiksihakim0@gmail.com>
2023-10-26 18:57:16 +0100

    add product almost done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,15 @@ import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import db from './firebase-config';
 import ProductOverview from './pages/productOverview/ProductOverview';
-
+import AddProduct from './pages/addProduct/AddProduct.jsx';
 const App = () => {
   return (
       <div>
     <Routes>
         <Route path="/"/>
         <Route path="/productsOverview" element={<ProductOverview/>}/>
+        <Route path="/add" element={<AddProduct/>}/>
+
     </Routes>
       </div>
   );

2262b0bd7925566dba44a4c259a1caff6cd38a55
irma-ahmed <ahmedirmani@yahoo.fr>
2023-10-26 15:08:17 +0100

    final repo

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
-import React, {useEffect, useState} from 'react'
-import db from './firebase-config'
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import db from './firebase-config';
 
 const App = () => {
-
-
   return (
-    <div>
-    hello world
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/"/>
+        </Switch>
       </div>
-  )
-}
+    </Router>
+  );
+};
 
 export default App

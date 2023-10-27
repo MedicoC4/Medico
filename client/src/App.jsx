e53682cd5ceee4e6ed577ca5a50a79a92d052d86
FediMaalej <fm3aalej@gmail.com>
2023-10-27 10:14:54 +0100

    50% done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import db from './firebase-config';
-import ProductOverview from './pages/productOverview/ProductOverview';
-
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import db from "./firebase-config";
+import ProductOverview from "./pages/productOverview/ProductOverview";
+import Home from './pages/landingPage/LandingPage'
 const App = () => {
   return (
-      <div>
-    <Routes>
-        <Route path="/"/>
-        <Route path="/productsOverview" element={<ProductOverview/>}/>
-    </Routes>
-      </div>
+    <div>
+      <Routes>
+        <Route path="/" />
+        <Route path="/productsOverview" element={<ProductOverview />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </div>
   );
 };
 
-export default App
+export default App;

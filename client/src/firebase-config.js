74db30534ad88018af04a97b05e844538e261d48
FediMaalej <fm3aalej@gmail.com>
2023-10-31 14:04:54 +0100

    fixing some errors

--- client/src/firebase-config.js
+++ client/src/firebase-config.js
@@ -3,16 +3,16 @@ import { getFirestore } from "@firebase/firestore";
 import { getAuth, createUserWithEmailAndPassword , GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCX4jPxFJQC5T1zDPzrKz3HRYQdeps2St4",
-  authDomain: "medico-5add3.firebaseapp.com",
-  projectId: "medico-5add3",
-  storageBucket: "medico-5add3.appspot.com",
-  messagingSenderId: "468442720217",
-  appId: "1:468442720217:web:4346fe0419451e1fe39bc8",
-  measurementId: "G-V475XKKJSM",
-};
+    apiKey: "AIzaSyCX4jPxFJQC5T1zDPzrKz3HRYQdeps2St4",
+    authDomain: "medico-5add3.firebaseapp.com",
+    projectId: "medico-5add3",
+    storageBucket: "medico-5add3.appspot.com",
+    messagingSenderId: "468442720217",
+    appId: "1:468442720217:web:4346fe0419451e1fe39bc8",
+    measurementId: "G-V475XKKJSM"
+  };
 
-const app = initializeApp(firebaseConfig);
+  const app = initializeApp(firebaseConfig);
 
 export default getFirestore(app);
 export const auth = getAuth(app);

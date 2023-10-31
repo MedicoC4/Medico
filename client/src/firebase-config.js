29a3f7bf6f2b533ed30b8facd56a6fd0e8556bf0
FediMaalej <fm3aalej@gmail.com>
2023-10-31 22:42:09 +0100

    add product to the database in progress

--- client/src/firebase-config.js
+++ client/src/firebase-config.js
@@ -1,19 +1,21 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "@firebase/firestore";
-import { getAuth, createUserWithEmailAndPassword , GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, createUserWithEmailAndPassword, GoogleAuthProvider } from "firebase/auth";
+import { getStorage } from "firebase/storage"; 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCX4jPxFJQC5T1zDPzrKz3HRYQdeps2St4",
-    authDomain: "medico-5add3.firebaseapp.com",
-    projectId: "medico-5add3",
-    storageBucket: "medico-5add3.appspot.com",
-    messagingSenderId: "468442720217",
-    appId: "1:468442720217:web:4346fe0419451e1fe39bc8",
-    measurementId: "G-V475XKKJSM"
-  };
+  apiKey: "AIzaSyCX4jPxFJQC5T1zDPzrKz3HRYQdeps2St4",
+  authDomain: "medico-5add3.firebaseapp.com",
+  projectId: "medico-5add3",
+  storageBucket: "medico-5add3.appspot.com",
+  messagingSenderId: "468442720217",
+  appId: "1:468442720217:web:4346fe0419451e1fe39bc8",
+  measurementId: "G-V475XKKJSM"
+};
 
-  const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-export default getFirestore(app);
+export const firestore = getFirestore(app);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+export const storage = getStorage(app); 

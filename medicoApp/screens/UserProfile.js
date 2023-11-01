1f374b4392f171f079fc7c0d3d3962f9a2166706
yjandoubi95 <58791201+yjandoubi95@users.noreply.github.com>
2023-11-01 09:15:36 +0100

    usProf

--- medicoApp/screens/UserProfile.js
+++ medicoApp/screens/UserProfile.js
@@ -1,203 +1,151 @@
+
+
+
 import React from 'react';
 import {
   StyleSheet,
   SafeAreaView,
-  ScrollView,
   View,
   Text,
   TouchableOpacity,
-  Image,
-  Switch,
 } from 'react-native';
-import FeatherIcon from 'react-native-vector-icons/Feather';
 
-const SECTIONS = [
+const items = [
   {
-    icon: 'settings',
-    items: [
-        { icon: 'users', color: '#fe9400', label: 'Personel Details', type: 'link' },
-        { icon: 'navigation', color: '#fe9400', label: 'Payment', type: 'link' },
-        { icon: 'navigation', color: '#fe9400', label: 'Settings', type: 'link' },
-        { icon: 'mail', color: '#fe9400', label: 'Support', type: 'link' },
-    ],
+    label : 'Patient',
+    description : 'Register as a Patient so you can find pharmacies and purchase drugs',
+    icon : ''
   },
-//   {
-//     header: 'Help',
-//     icon: 'help-circle',
-//     items: [
-//       { icon: 'flag', color: '#8e8d91', label: 'Report Bug', type: 'link' },
-//       { icon: 'mail', color: '#007afe', label: 'Contact Us', type: 'link' },
-//     ],
-//   },
-//   {
-//     header: 'Content',
-//     icon: 'align-center',
-//     items: [
-//       { icon: 'save', color: '#32c759', label: 'Saved', type: 'link' },
-//       { icon: 'download', color: '#fd2d54', label: 'Downloads', type: 'link' },
-//     ],
-//   },
-];
+  {
+    label : 'Pharmacy',
+    description : 'Register as a Pharmacy so you can sell and manage your drugs on the app.',
+    icon : ''
+  },
+  {
+    label : 'Doctor',
+    description : 'Register as a Doctor so you can ...',
+    icon : ''
+  }
+]
 
-export default function Example() {
+export default function UserProfile({ navigation }) {
+  const [value, setValue] = React.useState(0);
+
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <ScrollView contentContainerStyle={styles.container}>
-        <View style={styles.profile}>
-          <TouchableOpacity
-            onPress={() => {
-              // handle onPress
-            }}>
-            <View style={styles.profileAvatarWrapper}>
-              <Image
-                alt=""
-                source={{
-                  uri: 'https://scontent.ftun15-1.fna.fbcdn.net/v/t1.6435-9/199500660_1891527394346712_7737615867335763689_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=be3454&_nc_ohc=japbad3AvBkAX9AwDiI&_nc_ht=scontent.ftun15-1.fna&oh=00_AfC8omZ5xApf-5uL22Wv6wy_yKLy1UmcbdfUw9qV5nHPLA&oe=6567A682',
-                }}
-                style={styles.profileAvatar}
-              />
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#f6f6f6' }}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Select your account type</Text>
 
-              <TouchableOpacity
-                onPress={() => {
-                  // handle onPress
-                }}>
-                <View style={styles.profileAction}>
-                  <FeatherIcon color="#fff" name="edit-3" size={15} />
+        {items.map(({ label, description, icon }, index) => {
+          const isActive = value === index;
+          return (
+            <TouchableOpacity
+              key={index}
+              onPress={() => {
+                setValue(index);
+              }}>
+              <View style={[styles.radio, isActive && styles.radioActive]}>
+                <Text style={styles.radioLabel}>{label}</Text>
+
+    
+                <View style={styles.radioBadge}>
+                  <Text style={styles.radioBadgeText}>{icon}</Text>
                 </View>
-              </TouchableOpacity>
-            </View>
-          </TouchableOpacity>
 
-          <View style={styles.profileBody}>
-            <Text style={styles.profileName}>Yassin Jandoubi</Text>
+                <Text style={styles.radioDescription}>{description}</Text>
 
-            <Text style={styles.profileAddress}>
-              yjandoubi95@gmail.com
-            </Text>
-          </View>
-        </View>
-
-        {SECTIONS.map(({ header, items }) => (
-          <View style={styles.section} key={header}>
-            <Text style={styles.sectionHeader}>{header}</Text>
-            {items.map(({ label, icon, type, value, color }, index) => {
-              return (
-                <TouchableOpacity
-                  key={label}
-                  onPress={() => {
-                    // handle onPress
-                  }}>
-                  <View style={styles.row}>
-                    <View style={[styles.rowIcon, { backgroundColor: color }]}>
-                      <FeatherIcon color="#fff" name={icon} size={18} />
-                    </View>
-
-                    <Text style={styles.rowLabel}>{label}</Text>
-
-                    <View style={styles.rowSpacer} />
-
-                    {type === 'boolean' && <Switch value={value} />}
-
-                    {type === 'link' && (
-                      <FeatherIcon
-                        color="#0c0c0c"
-                        name="chevron-right"
-                        size={22}
-                      />
-                    )}
-                  </View>
-                </TouchableOpacity>
-              );
-            })}
-          </View>
-        ))}
-      </ScrollView>
+                <View
+                  style={[
+                    styles.radioInput,
+                    isActive && styles.radioInputActive,
+                  ]}
+                />
+              </View>
+            </TouchableOpacity>
+          );
+        })}
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    paddingVertical: 24,
-  },
-  section: {
-    paddingHorizontal: 24,
-  },
-  sectionHeader: {
-    paddingVertical: 12,
-    fontSize: 12,
-    fontWeight: '600',
-    color: '#9e9e9e',
-    textTransform: 'uppercase',
-    letterSpacing: 1.1,
-  },
-  profile: {
     padding: 24,
-    backgroundColor: '#fff',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
   },
-  profileAvatar: {
-    width: 72,
-    height: 72,
-    borderRadius: 9999,
-  },
-  profileAvatarWrapper: {
-    position: 'relative',
-  },
-  profileAction: {
-    position: 'absolute',
-    right: -4,
-    bottom: -10,
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: 28,
-    height: 28,
-    borderRadius: 9999,
-    backgroundColor: '#007bff',
-  },
-  profileName: {
-    marginTop: 20,
-    fontSize: 19,
-    fontWeight: '600',
-    color: '#414d63',
-    textAlign: 'center',
-  },
-  profileAddress: {
-    marginTop: 5,
-    fontSize: 16,
-    color: '#989898',
-    textAlign: 'center',
-  },
-  row: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    height: 50,
-    backgroundColor: '#f2f2f2',
-    borderRadius: 8,
+  title: {
+    fontSize: 32,
+    fontWeight: '700',
+    color: '#1d1d1d',
     marginBottom: 12,
-    paddingLeft: 12,
-    paddingRight: 12,
+    alignItems:'center',
   },
-  rowIcon: {
-    width: 32,
-    height: 32,
+  radio: {
+    marginTop :30 ,
+    position: 'relative',
+    backgroundColor: '#fff',
+    marginBottom: 12,
+    padding: 16,
+    borderRadius: 25,
+    alignItems: 'flex-start',
+    borderWidth: 2,
+    borderColor: 'transparent',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+    elevation: 2,
+  },
+  radioActive: {
+    borderColor: "#007260",
+  },
+  radioLabel: {
+    fontSize: 18,
+    fontWeight: '600',
+    letterSpacing: 1.2,
+    color: "#007260",
+    textTransform: 'uppercase',
+    marginBottom: 8,
+  },
+  radioPrice: {
+    fontSize: 18,
+    fontWeight: '700',
+    color: '#2f2f2f',
+    marginBottom: 12,
+  },
+  radioBadge: {
+    backgroundColor: "#007260",
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    borderRadius: 6,
+    marginBottom: 12,
+  },
+  radioBadgeText: {
+    fontSize: 15,
+    fontWeight: '500',
+    color:"#007260",
+  },
+  radioDescription: {
+    fontSize: 15,
+    fontWeight: '500',
+    color: '#848a96',
+  },
+  radioInput: {
+    position: 'absolute',
+    top: 16,
+    right: 16,
+    width: 24,
+    height: 24,
     borderRadius: 9999,
-    marginRight: 12,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
+    borderWidth: 2,
+    borderColor: "#007260",
   },
-  rowLabel: {
-    fontSize: 17,
-    fontWeight: '400',
-    color: '#0c0c0c',
-  },
-  rowSpacer: {
-    flexGrow: 1,
-    flexShrink: 1,
-    flexBasis: 0,
+  radioInputActive: {
+    borderWidth: 7,
+    borderColor: "#007260",
   },
 });

0ca99d2424d204369632f485187098d4f690744d
Le Ash <ksiksihakim0@gmail.com>
2023-11-03 09:22:52 +0100

    pull request

--- medicoApp/App.js
+++ medicoApp/App.js
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName='DoctorPdf'
+        initialRouteName='FirstStep'
       >
         <Stack.Screen
           name="Welcome"

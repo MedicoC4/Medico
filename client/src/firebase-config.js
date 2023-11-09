d142946c2a3c170a745127089b81f5990a3687fd
FediMaalej <fm3aalej@gmail.com>
2023-11-09 14:37:05 +0100

    login completed

--- client/src/firebase-config.js
+++ client/src/firebase-config.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCX4jPxFJQC5T1zDPzrKz3HRYQdeps2St4",
@@ -17,5 +17,4 @@ const app = initializeApp(firebaseConfig);
 
 export const DB = getFirestore(app);
 export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();
 export const storage = getStorage(app)

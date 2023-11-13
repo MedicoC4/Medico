851cd86a360a8db5ca51c9f6ffc62529326f29f4
aymenmakhlouff <makhloufaymen.fr@gmail.com>
2023-11-13 17:53:02 +0100

    save

--- server/controller/nodemailer.controller.js
+++ server/controller/nodemailer.controller.js
@@ -2,19 +2,12 @@ const nodemailer = require("nodemailer");
 
 module.exports = {
     sendMail: async(req,res)=>{
-        try {
-            const transporter = nodemailer.createTransport({
-                host: "smtp.forwardemail.net",
-                port: 465,
-                secure: true,
-                auth: {
-                  // TODO: replace `user` and `pass` values from <https://forwardemail.net>
-                  user: "REPLACE-WITH-YOUR-ALIAS@YOURDOMAIN.COM",
-                  pass: "REPLACE-WITH-YOUR-GENERATED-PASSWORD",
-                },
-              });
-        } catch (error) {
-            
+        let config = {
+            service:"gmail",
+            auth:{
+                user:"",
+                pass:""
+            }
         }
     }
 }

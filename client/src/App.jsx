b42c14b02c4e8fe249e77045ba3540399305f553
FediMaalej <fm3aalej@gmail.com>
2023-11-16 14:38:11 +0100

    loading is ready

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState, useContext, lazy, Suspense } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -8,30 +8,37 @@ import {
   Navigate,
 } from "react-router-dom";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
-import ProductOverview from "./pages/productOverview/ProductOverview";
-import AddProduct from "./pages/addProduct/AddProduct.jsx";
-import Overview from "./pages/overview/Overview";
-import Home from "./pages/dashboard/LandingPage";
-import ProductDetails from "./pages/productDetails/ProductDetails";
-import Statistics from "./pages/statistics/Statistics";
-import SideNav from "./components/sideNav/SideNav";
-import ResetePassword from "./pages/resetePassword/ResetePassword";
-import Landing from "./pages/landingPage/landing";
-import Ordering from "./pages/ordering/OrderList.jsx";
 import { gsap } from "gsap-trial";
 import { ScrollTrigger } from "gsap-trial/ScrollTrigger";
 import { ScrollSmoother } from "gsap-trial/ScrollSmoother";
 import { TransitionProvider } from "./context/TransitionContext.jsx";
 import TransitionComponent from "./components/Transition/Transition.jsx";
 import { AuthContext } from "./context/AuthContext.js";
-import OderDetails from "./pages/ordering/OderDetails.jsx";
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+// Lazy-loaded components
+const Landing = lazy(() => import("./pages/landingPage/landing"));
+const Home = lazy(() => import("./pages/dashboard/LandingPage"));
+const AddProduct = lazy(() => import("./pages/addProduct/AddProduct.jsx"));
+const ProductOverview = lazy(() =>
+  import("./pages/productOverview/ProductOverview")
+);
+const ProductDetails = lazy(() =>
+  import("./pages/productDetails/ProductDetails")
+);
+const Statistics = lazy(() => import("./pages/statistics/Statistics"));
+const ResetePassword = lazy(() =>
+  import("./pages/resetePassword/ResetePassword")
+);
+const Ordering = lazy(() => import("./pages/ordering/OrderList.jsx"));
+const OderDetails = lazy(() => import("./pages/ordering/OderDetails.jsx"));
+
 const App = () => {
   const location = useLocation();
-
   const { currentUser } = useContext(AuthContext);
   console.log(currentUser);
+
   const RequireAuth = ({ children }) => {
     return currentUser ? children : <Navigate to="/dashboard" />;
   };
@@ -53,7 +60,6 @@ const App = () => {
 
   return (
     <div>
-      {/* <SideNav /> */}
       <TransitionGroup>
         <CSSTransition
           key={location.key}
@@ -61,27 +67,38 @@ const App = () => {
           timeout={300}
           appear
         >
-          <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route
-              path="/dashboard"
-              element={
-                <RequireAuth>
-                  <Home />
-                </RequireAuth>
-              }
-            />
-            <Route path="add-product" element={<AddProduct />} />
-            <Route path="products" element={<ProductOverview />} />
-            <Route
-              path="product-detail/:productId"
-              element={<ProductDetails />}
-            />
-            <Route path="statistics" element={<Statistics />} />
-            <Route path="reset-password" element={<ResetePassword />} />
-            <Route path="orders" element={<Ordering />} />
-            <Route path="order-details/:orderId" element={<OderDetails />} />
-          </Routes>
+          <Suspense
+            fallback={
+              <div class="loader">
+                <div class="circle"></div>
+                <div class="circle"></div>
+                <div class="circle"></div>
+                <div class="circle"></div>
+              </div>
+            }
+          >
+            <Routes>
+              <Route path="/" element={<Landing />} />
+              <Route
+                path="/dashboard"
+                element={
+                  <RequireAuth>
+                    <Home />
+                  </RequireAuth>
+                }
+              />
+              <Route path="add-product" element={<AddProduct />} />
+              <Route path="products" element={<ProductOverview />} />
+              <Route
+                path="product-detail/:productId"
+                element={<ProductDetails />}
+              />
+              <Route path="statistics" element={<Statistics />} />
+              <Route path="reset-password" element={<ResetePassword />} />
+              <Route path="orders" element={<Ordering />} />
+              <Route path="order-details/:orderId" element={<OderDetails />} />
+            </Routes>
+          </Suspense>
         </CSSTransition>
       </TransitionGroup>
     </div>
